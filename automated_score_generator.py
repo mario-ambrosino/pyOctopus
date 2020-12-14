9ec91b313b3678832c4bd9185cbfb4d57a18096f
Mario Ambrosino <mario.ambrosino7@gmail.com>
2020-12-14 23:41:15 +0100

    Completed Script for automated scores

--- automated_score_generator.py
+++ automated_score_generator.py
@@ -1,33 +1,58 @@
-# import sys
-# import uuid
-# import time
+"""
+Script to automate the score comparison with a simple grid search.
 
-def main():
-    import octolib.helper as hp
+TODO EXTEND TUTORIAL:
 
-    threshold_set = [1,2]
-    detection_range_set = [0.2, 0.3, 0.5, 0.6]
-    e_set = [15]
-    ms_set = [15]
-    cm_set = ["cityblock"]
+Example of usage:
 
-    hp.automate_score_generation(
-        threshold_set = threshold_set,
-        detection_range_set = detection_range_set,
-        e_set = e_set,
-        ms_set = ms_set,
-        cm_set = cm_set,
-        mode = "grid_search"
-        )
+python3 automated_score_generator.py --THR "0.1; 0.5" --DER "0.1; 0.4" --EPS "15" --MIS "15" --CME "cityblock ; manhattan"
 
+
+"""
+
+import time
+import argparse
+import octolib.helper as hp
 
 if __name__ == '__main__':
-    # try:
+    try:
+        # Start Logging
         # sys.stdout = open("log/{}.log".format(uuid.uuid4()), "w")
-        # print("[{}] # ".format(time.ctime()) + "Program Started. Logging Started")
-    main()
+        print("[{}] # ".format(time.ctime()) + "Logging Started")
+
+        # Parse Input
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--THR", type = str, default = 0, help = "list(float): Threshold - from 0.1 to 3")
+        parser.add_argument("--DER", type = str, default = 0, help = "list(float): Detection Range [m] - from 0.1 to 5")
+        parser.add_argument("--EPS", type = str, default = 0, help = "list(int): Epsilon-Neighborhood for DBSCAN")
+        parser.add_argument("--MIS", type = str, default = 0, help = "list(int): Min Samples for DBSCAN")
+        parser.add_argument("--CME", type = str, default = 0, help = "list(strings): Metric to choose for DBSCAN")
+        opt = parser.parse_args()
+
+        # Assign Grid delimiters
+        threshold_set = [float(s) for s in opt.THR.replace(" ", "").split(";")]
+        detection_range_set = [float(s) for s in opt.DER.replace(" ", "").split(";")]
+        e_set = [float(s) for s in opt.EPS.replace(" ", "").split(";")]
+        ms_set = [float(s) for s in opt.MIS.replace(" ", "").split(";")]
+        cm_set = [str(s) for s in opt.CME.replace(" ", "").split(";")]
+
+        print("[{}] # ".format(time.ctime()) + "Script Automated Score Generator Started. \n\nPARAMETERS :: \nTHR:"
+                                               " {}\nDER: {}\nEPS: {}\nMIS: {}\nCME: {}\n".format(
+            threshold_set, detection_range_set, e_set, ms_set, cm_set
+            ))
+
+        # Generate Scores
+        hp.automate_score_generation(
+            threshold_set = threshold_set,
+            detection_range_set = detection_range_set,
+            e_set = e_set,
+            ms_set = ms_set,
+            cm_set = cm_set,
+            mode = "grid_search"
+            )
+
         # sys.stdout.close()
-    # except KeyboardInterrupt:
-        # print("[{}] # ".format(time.ctime()) + "Interrupted by User. Closing Log and exiting.")
+    except KeyboardInterrupt:
+        print("[{}] # ".format(time.ctime()) + "Interrupted by User. Closing Log and exiting.")
         # sys.stdout.close()
         # sys.exit(0)
