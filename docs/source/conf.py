c4cc89d812c05915ef5614a3933826a310bbc79f
Mario Ambrosino <mario.ambrosino7@gmail.com>
2020-12-14 15:19:47 +0100

    4th RTD test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,6 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
 # Mock modules
 autodoc_mock_imports = ["pywt"]
+
+
